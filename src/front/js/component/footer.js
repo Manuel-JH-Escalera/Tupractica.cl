4adcc2d65803535d174582423840117dec03502e
AnyelinaParedes <apg15194@hotmail.com>
2022-05-12 20:55:41 +0000

    frontPage,Card, Footer,Jumbotron,Navbar

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,114 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <div className="container">
+    <footer className="row row-cols-5 py-5 my-5 border-top">
+      <div className="col">
+        <a
+          href="/"
+          className="d-flex align-items-center mb-3 link-dark text-decoration-none"
+        >
+          <svg className="bi me-2" width="40" height="32">
+            hola
+          </svg>
+        </a>
+        <p className="text-muted">&copy; 2021</p>
+      </div>
+
+      <div className="col"></div>
+
+      <div className="col">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              Home
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              Features
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              Pricing
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              FAQs
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              About
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="col">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              Home
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              Features
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              Pricing
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              FAQs
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              About
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="col">
+        <h5>Section</h5>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              Home
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              Features
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              Pricing
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              FAQs
+            </a>
+          </li>
+          <li className="nav-item mb-2">
+            <a href="#" className="nav-link p-0 text-muted">
+              About
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  </div>
 );
---
+++ src/front/js/pages/UserProfile.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const UserProfile = () => {
+  return <h1>hola</h1>;
+};
