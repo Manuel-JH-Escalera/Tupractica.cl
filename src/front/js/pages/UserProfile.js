460458017849853d27c7a345edda1f915049669c
Attackama-Bwoy <manuel.jh.escalera@gmail.com>
2022-05-16 00:51:11 +0000

    user profile route

--- src/front/js/pages/UserProfile.js
+++ src/front/js/pages/UserProfile.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Navbar } from "../component/navbar";
+import { Navbar } from "../component/Navbar";
 import { Footer } from "../component/footer";
 import ProfilePhoto from "../component/ProfilePhoto";
 import ProfileDataPerson from "../component/ProfileDataPerson";
 import AddressData from "../component/AddressData";
 import ContacInformation from "../component/ ContactInformation";
 import Anexos from "../component/anexos";
+
 export const UserProfile = () => {
   return (
-    <>
-      <Navbar />
-
       <div className="container ">
         <ProfilePhoto />
         <ProfileDataPerson />
@@ -18,7 +16,5 @@ export const UserProfile = () => {
         <ContacInformation />
         <Anexos />
       </div>
-      <Footer />
-    </>
   );
 };
