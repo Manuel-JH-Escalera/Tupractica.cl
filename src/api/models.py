7230c73bf908d248662cd8387ae24c5f9dfcb8f2
crisepu <csepulvedavenegas@gmail.com>
2022-05-16 02:21:44 +0000

    inicio clases BBDD

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,84 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
-class User(db.Model):
+class Practicante(db.Model):
+    __tablename__ = 'practicante'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.email}>'
+    nombre = db.Column(db.String(120), nullable=False)
+    apellido = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False)
+    fecha_nacimiento = db.Column(db.datetime.date(), nullable=False)
+    institucion = db.Column(db.String(120), nullable=False)
+    foto_perfil = db.Column(db.String(120), nullable=True)
+    biografia = db.Column(db.String(500), nullable=True)
+    carrera_estudio = db.Column(db.String(120), nullable=False)
+    telefono = db.Column(db.Integer, nullable=False)
+    comuna = db.Column(db.String(120), nullable=False)
+    contacts = db.relationship('Contact', backref="user")
+    
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username
         }
+    
+    def serialize_with_contacts(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "contacts": self.get_contacts()
+        }
+
+    def get_contacts(self):
+        return list(map(lambda contact: contact.serialize(), self.contacts))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Contact(db.Model):
+    __tablename__ = 'contacts'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120))
+    phone = db.Column(db.String(120), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "phone": self.phone
+        }
+
+    def serialize_with_user(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "phone": self.phone,
+            "user": self.user.username
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
